13064b4eb2169732a61f111b9dc1384874edbdeb
jeremy <saveries.j@gmail.com>
2025-03-15 23:46:18 +0100

    page accueil fait avec le css pour version desktop

--- front/src/App.svelte
+++ front/src/App.svelte
@@ -1,47 +1,135 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
-</script>
+// @ts-nocheck
 
-<main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  import { Router, Link, Route } from "svelte-routing";
+  import CardWithModel from "./utils/cardWithModal.svelte";
+  import ContactForm from "./utils/contactForm.svelte";
+  import Softskill from "./utils/softskill.svelte";
+    </script>
 
-  <div class="card">
-    <Counter />
-  </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+
+<header>
+        <div class="headerContainer">
+          <ul class="titleHeader">
+            <li class="firstName">jeremy</li>
+            <li class="lastName">saveries</li>
+          </ul>
+        </div>
+      </header>
+      
+     
+      
+      <main>
+        <div class="mainContainer">
+          <div class="titlePoste">
+            <h2 class="postName">Développeur Full Stack</h2>
+          </div>
+      
+          <div class="mainContent">
+            <div class="présentation">
+              <p>
+                À 34 ans, je suis actuellement en reconversion pour travailler dans le milieu du développement web. Suite à un bac +2 Développeur Web à l'école Oclock, j’ai acquis les bases de plusieurs langages web (HTML, CSS, JavaScript), la gestion de bases de données ainsi que l'utilisation de logiciels tels que Figma, que j’utilise régulièrement pour créer des maquettes. J’ai ensuite pu exercer et améliorer ces compétences lors d’un stage d’un mois aux côtés de développeurs web expérimentés.
+
+                Aujourd’hui, je suis à la recherche d’un CDI en tant que développeur full stack, et je propose également mes services en freelance pour des projets web.
+                
+                Vous pouvez télécharger mon CV ci-dessous et me contacter directement via le formulaire de contact plus bas.
+              </p>
+            </div>
+      
+            <div class="photoProfil">
+              <img class="photo" src="public/img/profil.jpg" alt="photo de profil" />
+            </div>
+          </div>
+      
+          <div class="inputContainer">
+            <a href="/public/document/cvjeremy5.pdf" download="cvjeremysaveries">
+              <button class="inputButton">Télécharger mon CV</button>
+            </a>
+      
+            <div class="flexLogo">
+              <ul class="logoList">
+                <li class="logoLinkedin">
+                  <a href="https://www.linkedin.com/in/jérémy-saveries">
+                    <i class="fa-brands fa-linkedin"></i>
+                  </a>
+                </li>
+                <li class="logoGitHub">
+                  <a href="https://github.com/jeremysaveries">
+                    <i class="fa-brands fa-github"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+      
+          <section>
+            <h2 class="softskillTittle">Mes compétences</h2>
+            <Softskill />
+          </section>
+      
+          <section>
+            <div class="projets">
+              <h2 class="projetTittle">Mes projets</h2>
+            </div>
+          </section>
+      
+          <section>
+            <div class="cardContainer">
+              <CardWithModel
+                imageSrc="/img/new logo.png"
+                imageAlt="logo du projet"
+                title="Site PetFoster Connect"
+                modalContent="Pet Foster Connect est un site fictif..."
+                modalLinkText="Voir plus"
+                modalLink="https://github.com/mon-projet/petfoster-connect"
+              />
+              <CardWithModel
+                imageSrc="/img/gameCollection.png"
+                imageAlt="logo d'un autre projet"
+                title="Game Collection"
+                modalContent="Game Collection est une application dédiée aux passionnés de jeux vidéo..."
+                modalLinkText="Voir plus"
+                modalLink=""
+              />
+            </div>
+          </section>
+      
+          <section>
+            <div class="contactForm">
+              <h2 class="contactTitle">Contactez-moi</h2>
+              <ContactForm />
+            </div>
+          </section>
+        </div>
+      
+        
+      
+      <footer>
+        <div class="footerContainer">
+          <h3 class="footerTitle">by jeremy.</h3>
+          <p>© jeremysaveriesdev.com. All rights reserved</p>
+        </div>
+      </footer>
+      
+      <style>
+    
+       @import url("/css/reset.css");
+        @import url("/css/style.css");
+        @import url("https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap");
+        @import url("https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css");
+        @import "@fortawesome/fontawesome-free/css/all.min.css";
+      
+        .cardContainer {
+          display: flex;
+          gap: 1rem;
+          flex-wrap: wrap;
+        }
+        .modalContent {
+          color: black;
+        }
+      </style>
 </main>
 
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+
